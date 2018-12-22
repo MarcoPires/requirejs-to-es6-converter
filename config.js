23343cb293fe394ecfe6cf771fbbcbe48b5a6bc7
Marco Pires <>
2018-12-22 19:23:42 +0000

    project bootstrap

---
+++ config.js
@@ -0,0 +1,48 @@
+module.exports = {
+	fileDependencies: {
+		alias: {
+			ignore: ['eventbroker', 'associations'],
+			replace: {
+				'jquery': {
+					path: 'vendor/js/jquery',
+					rootRelative: true
+				},
+				'underscore': {
+					path: 'vendor/js/underscore',
+					rootRelative: true
+				},
+				'bangular': {
+					path: 'vendor/js/bangular',
+					rootRelative: true
+				},
+				'backbone': {
+					path: 'vendor/js/backbone',
+					rootRelative: true
+				},
+				'moment': {
+					path: 'vendor/js/moment',
+					rootRelative: true
+				},
+				'q': {
+					path: 'vendor/js/q',
+					rootRelative: true
+				},
+				'select': {
+					path: 'vendor/js/bootstrap-select',
+					rootRelative: true
+				}
+			}
+		},
+
+		plugins: {
+			replace: {
+				'i18n!nls/leap_translations': {
+					path: 'translations',
+					rootRelative: true
+				}
+			}
+		}
+	},
+	entryFilePath: 'D:/Trabalho/Leya/leap-ui/dev_online/js/library/controllers/LibraryController.js',
+	projectRootDir: 'library'
+};
